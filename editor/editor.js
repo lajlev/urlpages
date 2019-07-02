d76105e5e2b7b47a6faa1676fede7728988821a2
Michael Lajlev <lajlev@gmail.com>
2019-07-02 14:09:42 +0200

    fix share link

--- editor/editor.js
+++ editor/editor.js
@@ -37,7 +37,7 @@ ${data["html"]}
 
 /* Return a link to view the page */
 function getViewLink(pageData) {
-  return `http://jstrieb.github.io/urlpages/#${window.btoa(pageData)}`;
+  return `http://share.lillefar.dk/#${window.btoa(pageData)}`;
 }
 
 
